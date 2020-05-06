1d360b8f405be58b9f09a8e86279e2c0836758e4
kiku ishimoto <kikuish@gmail.com>
2020-05-06 22:06:28 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -19,3 +19,10 @@ def swap_elements(array)
     array[1], array[2] = array[2], array[1]
     array
 end
+def reverse_array(array)
+  array.reverse
+end
+def kesha_maker(array)
+  array.each do |word|
+    word[2] = $
+end
